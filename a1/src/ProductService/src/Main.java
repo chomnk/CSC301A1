de011439131f47df53bfa8a87511536cd0c5b8b3
chomnk <86968977+chomnk@users.noreply.github.com>
2025-01-30 22:04:23 -0500

    fix product service

--- a1/src/ProductService/src/Main.java
+++ a1/src/ProductService/src/Main.java
@@ -62,6 +62,7 @@ public class Main {
                 CREATE TABLE IF NOT EXISTS products (
                     id INTEGER PRIMARY KEY,
                     productname VARCHAR(255),
+                    description VARCHAR(255),
                     price FLOAT,
                     quantity INT
                 );
@@ -94,6 +95,9 @@ public class Main {
         }
 
         public void handlePost(HttpExchange exchange) throws IOException {
+            JsonObject empty = new JsonObject();
+            String emptyResponse = gson.toJson(empty);
+
             if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) {
                 String requestBody = getRequestBody(exchange);
                 try {
@@ -102,42 +106,73 @@ public class Main {
                     if (json.has("command")) {
                         String command = json.get("command").getAsString();
                         if (json.has("id")) {
-                            int id = json.get("id").getAsInt();
+                            Integer id = json.has("id") ? json.get("id").getAsInt() : null;
                             String productName = json.has("productname") ? json.get("productname").getAsString() : null;
+                            String description = json.has("description") ? json.get("description").getAsString() : null;
                             Double price = json.has("price") ? json.get("price").getAsDouble() : null;
-                            Integer quantity = json.has("quantity") ? json.get("quantity").getAsInt() : null;
 
-                            boolean success = false;
+                            if (!json.has("quantity") || !json.get("quantity").isJsonPrimitive() ||
+                                    !json.get("quantity").getAsJsonPrimitive().isNumber() ||
+                                    json.get("quantity").getAsDouble() % 1 != 0) {
+
+                                sendResponse(exchange, 400, emptyResponse); // invalid quantity type
+                                return;
+                            }
+
+                            int quantity = json.get("quantity").getAsInt();
 
                             switch (command) {
-                                case "create":
-                                    if (productName != null && price != null && quantity != null) {
-                                        success = create(id, productName, price, quantity);
+                                case "create" -> {
+                                    if (
+                                            productName != null
+                                            && productName.length() != 0 // empty name
+                                            && price != null
+                                            && price >= 0 // negative price
+                                            && description != null // missing description
+                                            && quantity >= 0 // negative quantity
+                                    ) {
+                                        create(exchange, id, productName, description, price, quantity);
                                     } else {
-                                        sendResponse(exchange, "Missing required fields for creation.");
+                                        sendResponse(exchange, 400, emptyResponse);
                                     }
-                                    break;
-                                case "update":
-                                    success = update(id, productName, price, quantity);
-                                    break;
-                                case "delete":
-                                    success = delete(id, productName, price, quantity);
-                                    break;
-                            }
+                                }
 
-                            if (success) {
-                                sendResponse(exchange, "Success.");
-                            } else {
-                                sendResponse(exchange, "Failed.");
+                                case "update" -> {
+                                    if (
+                                            id != null // missing id
+                                            && description != null // missing description
+                                            && price != null
+                                            && price >= 0 // negative price
+                                            && quantity >= 0 // negative quantity
+                                    ) {
+                                        update(exchange, id, productName, description, price, quantity);
+                                    } else {
+                                        sendResponse(exchange, 400, emptyResponse);
+                                    }
+                                }
+
+                                case "delete" -> {
+                                    if (
+                                            productName != null // missing name field
+                                            && id != null // invalid id(actually in handler)
+                                    ) {
+                                        delete(exchange, id, productName, description, price, quantity);
+                                    } else {
+                                        sendResponse(exchange, 400, emptyResponse);
+                                    }
+                                }
+                                case "info" -> {
+                                    info(exchange, id, productName, description, price, quantity);
+                                }
                             }
                         } else {
-                            sendResponse(exchange, "No ID Provided.");
+                            sendResponse(exchange, 400, "No ID Provided.");
                         }
                     } else {
-                        sendResponse(exchange, "Command Error.");
+                        sendResponse(exchange, 404, "Command Error.");
                     }
                 } catch (Exception e) {
-                    sendResponse(exchange, "Error.");
+                    sendResponse(exchange, 404, "Error.");
                 }
             } else {
                 // Send a 405 Method Not Allowed response for non-POST requests
@@ -157,77 +192,178 @@ public class Main {
             }
         }
 
-        private static boolean create(int id, String name, double price, int quantity) {
-            String sql = "INSERT INTO products (id, productname, price, quantity) VALUES (?, ?, ?, ?);";
+        private static void info(HttpExchange exchange, int id, String name, String description, double price, int quantity) {
+            String sql = "SELECT description FROM products WHERE id = ?";
+
+            try (Connection conn = DriverManager.getConnection(DB_URL);
+                 PreparedStatement statement = conn.prepareStatement(sql)) {
+
+                statement.setInt(1, id);
+                ResultSet result = statement.executeQuery();
+
+                if (result.next()) {
+                    String productDescription = result.getString("description");
+
+                    JsonObject item = new JsonObject();
+                    item.addProperty("description", productDescription);
+                    String response = gson.toJson(item);
+                    try {
+                        sendResponse(exchange, 200, response);
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                } else {
+                    System.out.println("Product not found.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static void create(HttpExchange exchange, Integer id, String name, String description, double price, int quantity) {
+            String sql = "INSERT INTO products (id, productname, description, price, quantity) VALUES (?, ?, ?, ?, ?);";
             try (Connection conn = DriverManager.getConnection(DB_URL);
                  PreparedStatement statement = conn.prepareStatement(sql)) {
 
                 statement.setInt(1, id);
                 statement.setString(2, name);
-                statement.setDouble(3, price);
-                statement.setInt(4, quantity);
+                statement.setString(3, description);
+                statement.setDouble(4, price);
+                statement.setInt(5, quantity);
 
                 int affectedRows = statement.executeUpdate();
-                return affectedRows > 0;
+                if (affectedRows > 0) {
+                    JsonObject item = new JsonObject();
+                    item.addProperty("id", id);
+                    item.addProperty("productname", name);
+                    item.addProperty("description", description);
+                    item.addProperty("price", price);
+                    item.addProperty("quantity", quantity);
+                    String response = gson.toJson(item);
+                    try {
+                        sendResponse(exchange, 200, response);
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                }
             } catch (SQLException e) {
-                return false;
+                JsonObject item = new JsonObject();
+                String response = gson.toJson(item);
+                try {
+                    sendResponse(exchange, 409, response);
+                } catch (IOException e1) {
+                    System.out.println(e1);
+                }
             }
         }
 
-        private static boolean update(int id, String productname, Double price, Integer quantity) {
-            if (productname == null && price == null && quantity == null) { return false; }
+        private static void update(HttpExchange exchange, int id, String productname, String description, Double price, Integer quantity) {
+            JsonObject empty = new JsonObject();
+            String emptyResponse = gson.toJson(empty);
+
+            if (productname == null && description == null && price == null && quantity == null) {
+                try {
+                    sendResponse(exchange, 400, emptyResponse);
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+
             StringBuilder sql = new StringBuilder("UPDATE products SET ");
             if (productname != null) { sql.append("productname").append(" = ?, "); }
+            if (description != null) { sql.append("description").append(" = ?, "); }
             if (price != null) { sql.append("price").append(" = ?, "); }
             if (quantity != null) { sql.append("quantity").append(" = ?, "); }
             sql.setLength(sql.length() - 2);
             sql.append(" WHERE id = ?;");
 
             String query = sql.toString();
-            System.out.println(query);
             int count = 1;
 
             try (Connection conn = DriverManager.getConnection(DB_URL);
                  PreparedStatement statement = conn.prepareStatement(query)) {
 
                 if (productname != null) { statement.setString(count, productname); count++; }
+                if (description != null) { statement.setString(count, description); count++; }
                 if (price != null) { statement.setDouble(count, price); count++; }
                 if (quantity != null) { statement.setInt(count, quantity); count++; }
 
                 statement.setInt(count, id);
 
                 int affectedRows = statement.executeUpdate();
-                return affectedRows > 0;
-            } catch (SQLException e) {
-                return false;
+                if (affectedRows > 0) {
+                    JsonObject item = new JsonObject();
+                    item.addProperty("id", id);
+                    item.addProperty("productname", productname);
+                    item.addProperty("description", description);
+                    item.addProperty("price", price);
+                    item.addProperty("quantity", quantity);
+                    String response = gson.toJson(item);
+                    try {
+                        sendResponse(exchange, 200, response);
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                }
+            } catch (SQLException e1) {
+                JsonObject item = new JsonObject();
+                String response = gson.toJson(item);
+                try {
+                    sendResponse(exchange, 404, response);
+                } catch (IOException e2) {
+                    System.out.println(e1);
+                    System.out.println(e2);
+                }
             }
         }
 
-        private static boolean delete(int id, String name, Double price, Integer quantity) {
-            String check_sql = "SELECT COUNT(*) FROM products WHERE id = ? AND productname = ? AND price = ? AND quantity = ?";
+        private static void delete(HttpExchange exchange, int id, String name, String description, Double price, Integer quantity) {
+            String check_sql = "SELECT COUNT(*) FROM products WHERE id = ? AND productname = ? AND description = ? AND price = ? AND quantity = ?";
             String delete_sql = "DELETE FROM products WHERE id = ?";
 
             try (Connection conn = DriverManager.getConnection(DB_URL);
                  PreparedStatement check = conn.prepareStatement(check_sql);
                  PreparedStatement delete = conn.prepareStatement(delete_sql)) {
 
-                // Set parameters for the existence check
                 check.setInt(1, id);
                 check.setString(2, name);
-                check.setDouble(3, price);
-                check.setInt(4, quantity);
+                check.setString(3, description);
+                check.setDouble(4, price);
+                check.setInt(5, quantity);
 
-                // Execute the query
                 ResultSet result = check.executeQuery();
                 if (result.next() && result.getInt(1) > 0) {
                     delete.setInt(1, id);
                     int affectedRows = delete.executeUpdate();
-                    return affectedRows > 0;
+                    if (affectedRows > 0) {
+                        JsonObject item = new JsonObject();//empty
+                        String response = gson.toJson(item);
+                        try {
+                            sendResponse(exchange, 200, response);
+                        } catch (IOException e) {
+                            System.out.println(e);
+                        }
+                    }
                 } else {
-                    return false;
+                    // don't match
+                    JsonObject item = new JsonObject();
+                    String response = gson.toJson(item);
+                    try {
+                        sendResponse(exchange, 200, response);
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
                 }
             } catch (SQLException e) {
-                return false;
+                // invalid_id
+                JsonObject item = new JsonObject();
+                String response = gson.toJson(item);
+                try {
+                    sendResponse(exchange, 200, response);
+                } catch (IOException e1) {
+                    System.out.println(e);
+                    System.out.println(e1);
+                }
             }
         }
 
@@ -241,12 +377,15 @@ public class Main {
                 if (matcher.find()) {
                     ID = Integer.parseInt(matcher.group(1));
                 } else {
-                    sendResponse(exchange, "ID Format Error");
+                    // invalid type
+                    JsonObject empty = new JsonObject();
+                    String emptyResponse = gson.toJson(empty);
+                    sendResponse(exchange, 404, emptyResponse);
                 }
 
                 JsonObject item = new JsonObject();
 
-                String sql = "SELECT id, productname, price, quantity FROM products WHERE id = ?";
+                String sql = "SELECT id, productname, description, price, quantity FROM products WHERE id = ?";
                 try (Connection conn = DriverManager.getConnection(DB_URL);
                      PreparedStatement statement = conn.prepareStatement(sql)) {
                     statement.setInt(1, ID);
@@ -254,15 +393,24 @@ public class Main {
                     if (result.next()) {
                         item.addProperty("id", result.getInt("id"));
                         item.addProperty("productname", result.getString("productname"));
+                        item.addProperty("description", result.getString("description"));
                         item.addProperty("price", result.getDouble("price"));
                         item.addProperty("quantity", result.getInt("quantity"));
+                    } else {
+                        // no matching
+                        JsonObject empty = new JsonObject();
+                        String emptyResponse = gson.toJson(empty);
+                        sendResponse(exchange, 404, emptyResponse);
                     }
                 } catch (SQLException e) {
-                    sendResponse(exchange, "SQL Error.");
+                    // no matching
+                    JsonObject empty = new JsonObject();
+                    String emptyResponse = gson.toJson(empty);
+                    sendResponse(exchange, 404, emptyResponse);
                 }
 
                 String response = gson.toJson(item);
-                sendResponse(exchange, response);
+                sendResponse(exchange, 200, response);
             } else {
                 exchange.sendResponseHeaders(405,0);
                 exchange.close();
@@ -270,8 +418,8 @@ public class Main {
         }
     }
 
-    private static void sendResponse(HttpExchange exchange, String response) throws IOException {
-        exchange.sendResponseHeaders(200, response.length());
+    private static void sendResponse(HttpExchange exchange, int status, String response) throws IOException {
+        exchange.sendResponseHeaders(status, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes(StandardCharsets.UTF_8));
         os.close();
